a4afd228dde6feda02460eeff186ad350313789b
Gustavo Henrique <gustavohp4@gmail.com>
2022-03-21 20:21:52 -0300

    programacao defensiva e colorir pecas

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,7 @@ public class ChessPosition {
 	private int row;
 
 	public ChessPosition(char column, int row) {
-		if (column < 'a' || column > 'h' || row < 1 || row < 8)
+		if (column < 'a' || column > 'h' || row < 1 || row > 8)
 			throw new ChessException("Erro instanciando chessPosition.");
 		this.column = column;
 		this.row = row;
